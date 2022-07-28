37350d0565c88261313b0fad4b38f51647197f21
Daniel Morris <uptownmelo#gmail.com>
2022-07-28 14:31:59 -0400

    index.jsx view rendered at pokemon route

--- server.js
+++ server.js
@@ -3,15 +3,21 @@ const pokemon = require('./models/pokemon.js')
 const port = 3000
 const app = express()
 
+
+//setting up my views
+app.set('view engine', 'jsx'); //setting up our HTML template
+app.engine('jsx', require('express-react-views').createEngine()); //initializing our engine 
+
+//my routes
 app.get('/', function(req, res){
     res.send('Welcome to the Pokemon App!');
 }) 
 
 app.get('/pokemon', function(req,res){
-    res.send(pokemon)
+    res.render('Index')
 })
 
-
+//port 
 app.listen(port, function() {
     console.log('Listening on port 3000');
    })
---
+++ views/Index.jsx
@@ -0,0 +1,9 @@
+const React = require('react')
+class Index extends React.Component {
+   render () {
+    return (
+      <h1> 'See All The Pokemon!' </h1>
+     );
+    }
+ }
+ module.exports  = Index
