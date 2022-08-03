e40c0ea2af49dbadc41c73454b1ca23d2db8637f
Daniel Morris <uptownmelo#gmail.com>
2022-08-03 18:48:09 -0400

    Adding a New and Create Route and incorporating Mongoose JS

--- views/Show.jsx
+++ views/Show.jsx
@@ -16,7 +16,7 @@ class Show extends React.Component {
     <div style = {myStyle}>
       <h1> 'Gotta Catch 'Em All' </h1>
       <h2>{pokemon.name[0].toUpperCase() + pokemon.name.slice(1)}</h2>
-      <img src= {pokemon.img} />
+      <img src={pokemon.img} />
       <br/>
         <nav>
           <a href="/pokemon"> Back </a>
