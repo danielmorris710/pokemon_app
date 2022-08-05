7315f96fb5863aaebc503a204f0af75da18b71ce
Daniel Morris <uptownmelo#gmail.com>
2022-08-05 14:09:16 -0400

    fixed background

--- views/Show.jsx
+++ views/Show.jsx
@@ -1,5 +1,9 @@
 const React = require('react')
 
+const body = {
+  backgroundColor: '#ffcb05',
+}
+
 const myStyle = {
   color: '#2a75bb',
   backgroundColor: '#ffcb05',
@@ -13,15 +17,17 @@ class Show extends React.Component {
    render () {
     const { pokemon } = this.props
     return (
-    <div style = {myStyle}>
-      <h1> 'Gotta Catch 'Em All' </h1>
-      <h2>{pokemon.name}</h2>
-      <img src={`http://img.pokemondb.net/artwork/${pokemon.name.toLowerCase()}.jpg`}></img>
-      <br/>
-        <nav>
-          <a href="/pokemon"> Back </a>
-        </nav>
-    </div>
+    <html style = {body}>
+      <div style = {myStyle}>
+        <h1> 'Gotta Catch 'Em All' </h1>
+        <h2>{pokemon.name}</h2>
+        <img src={`http://img.pokemondb.net/artwork/${pokemon.name.toLowerCase()}.jpg`}></img>
+        <br/>
+          <nav>
+            <a href="/pokemon"> Back </a>
+          </nav>
+      </div>
+    </html>
      );
     }
  }
